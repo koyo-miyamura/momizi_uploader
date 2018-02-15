cf45948f17b80161209b9b0cf54c74c6ff3cd53d
koyo-miyamura <nexus9071@gmail.com>
2018-02-15 19:27:18 +0900

    rails g controller の実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/login'
+
+  get 'users/signup'
+
+  get 'contents/index'
+
+  get 'contents/detail'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
