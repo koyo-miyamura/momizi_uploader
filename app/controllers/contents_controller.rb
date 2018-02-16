1893ddd9aaeaa626703f48dae64b45821181ab8b
koyo-miyamura <nexus9071@gmail.com>
2018-02-16 18:33:54 +0900

    flashの簡易実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -7,15 +7,18 @@ class ContentsController < ApplicationController
   def new
     @content = Content.new(content_params)
     @content.user_id = 1
-    if @content.save!
+    if @content.valid?
+      @content.save
+      flash[:success] = "投稿できました"
       redirect_to action: :index
     else
-      render "Error"
+      flash[:error] = @content.errors.full_messages
+      redirect_to action: :index
     end
   end
 
   def detail
-    
+
   end
 
     private
