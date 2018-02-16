3999b58ae0a7f66ddf95e509bc7e561bd18f48a8
koyo-miyamura <nexus9071@gmail.com>
2018-02-16 16:32:13 +0900

    contents post機能の追加

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,7 +1,24 @@
 class ContentsController < ApplicationController
   def index
+    #@contents_list = Content.find(:all)
+    @new_content = Content.new
+  end
+
+  def new
+    @content = Content.new(content_params)
+    @content.user_id = 1
+    if @content.save!
+      redirect_to action: :index
+    else
+      render "Error"
+    end
   end
 
   def detail
   end
+
+    private
+    def content_params
+      params.require(:content).permit(:caption, :image)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   get 'users/signup'
 
-  get 'contents/index'
+  get  'contents/index'
+  post 'contents/new', to: 'contents#new', as: :contents_new
 
   get 'contents/detail'
 
