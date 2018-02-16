519a7eb592bf26a0848919e65476095095792284
koyo-miyamura <nexus9071@gmail.com>
2018-02-16 16:31:06 +0900

    contentsデータベースを image_path->image に変更

--- db/migrate/20180215095804_create_contents.rb
+++ db/migrate/20180215095804_create_contents.rb
@@ -2,7 +2,7 @@ class CreateContents < ActiveRecord::Migration[5.1]
   def change
     create_table :contents do |t|
       t.integer :user_id
-      t.string :image_path
+      t.string :image
       t.text :caption
       t.datetime :created_at
       t.datetime :deleted_at
